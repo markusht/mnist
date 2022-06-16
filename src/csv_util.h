4c72f7e42017d2cf217079c55823d576fce518d4
Markus Heinrich Toribio <markusht1@gmail.com>
2022-06-16 19:02:31 +0200

    add propagation

--- src/csv_util.h
+++ src/csv_util.h
@@ -9,22 +9,22 @@
 
 // typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> MatrixXi;
 
-Eigen::MatrixXi read_csv(const std::string& path) {
+Eigen::MatrixXd read_csv(const std::string& path) {
 	std::cerr << "Importing from csv: " << path << std::endl;
 	std::ifstream indata;
 	indata.open(path);
 	std::string line;
-	std::vector<int> values;
+	std::vector<double> values;
 	uint rows = 0;
 	while (std::getline(indata, line)) {
 		std::stringstream lineStream(line);
 		std::string cell;
 		while (std::getline(lineStream, cell, ',')) {
-			values.push_back(std::stoi(cell));
+			values.push_back(std::stod(cell));
 		}
 		++rows;
 	}
-	return Eigen::Map<Eigen::MatrixXi>(values.data(), rows, values.size()/rows);
+	return Eigen::Map<Eigen::MatrixXd>(values.data(), rows, values.size()/rows);
 }
 
 template<typename M>
--- src/main.cpp
+++ src/main.cpp
@@ -13,26 +13,83 @@ init_params() {
 	return {W1, b1, W2, b2};
 }
 
-std::tuple<Eigen::MatrixXi, Eigen::MatrixXi, Eigen::MatrixXi, Eigen::MatrixXi>
+std::tuple<Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd>
 read_data() {
 	const std::string trainPath = "../data/train.csv";
 	const std::string testPath = "../data/test.csv";
 
-	Eigen::MatrixXi train = read_csv(trainPath);
-	Eigen::MatrixXi test = read_csv(testPath);
+	Eigen::MatrixXd train = read_csv(trainPath);
+	Eigen::MatrixXd test = read_csv(testPath);
 
 	int m = train.rows(); // size of train data set
 	int n = train.cols(); // amount of pixels + 1
 
 	std::cerr << "Slicing raw data" << std::endl;
-	Eigen::MatrixXi data_dev = test.transpose();
-	Eigen::MatrixXi Y_dev = data_dev(0, Eigen::all);
-	Eigen::MatrixXi X_dev = data_dev(Eigen::seq(1,n-2), Eigen::all);
-	Eigen::MatrixXi data_train = train.transpose();
-	Eigen::MatrixXi Y_train = data_train(0, Eigen::all);
-	Eigen::MatrixXi X_train = data_train(Eigen::seq(1,n-1), Eigen::all);
+	Eigen::MatrixXd data_dev = test.transpose();
+	Eigen::MatrixXd Y_dev = data_dev(0, Eigen::all);
+	Eigen::MatrixXd X_dev = data_dev(Eigen::seq(1,n-2), Eigen::all);
+	Eigen::MatrixXd data_train = train.transpose();
+	Eigen::MatrixXd Y_train = data_train(0, Eigen::all);
+	Eigen::MatrixXd X_train = data_train(Eigen::seq(1,n-1), Eigen::all);
 
 	return {Y_dev, X_dev, Y_train, X_train};
+}
+
+Eigen::MatrixXd ReLU(Eigen::MatrixXd Z) {
+	return Z.cwiseMax(0);
+}
+
+Eigen::MatrixXd softmax(Eigen::MatrixXd Z) {
+	Eigen::MatrixXd m = Z.array().exp();
+	double sum = m.sum();
+	Eigen::MatrixXd n = m / sum;
+	return n;
+}
+
+std::tuple<Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd>
+forward_prop(Eigen::MatrixXd W1, Eigen::MatrixXd b1, Eigen::MatrixXd W2, Eigen::MatrixXd b2, Eigen::MatrixXd X) {
+	Eigen::MatrixXd Z1 = W1*X + b1;
+	Eigen::MatrixXd A1 = ReLU(Z1);
+	Eigen::MatrixXd Z2 = W2*A1 + b2;
+	Eigen::MatrixXd A2 = softmax(Z2);
+	return {Z1, A1, Z2, A2};
+}
+
+Eigen::MatrixXd one_hot(Eigen::MatrixXd Y) {
+	int m = Y.size();
+	Eigen::MatrixXd one_hot_Y = Eigen::MatrixXd::Zero(m, 10); // 10 for number of digits
+	Eigen::MatrixXd identity = Eigen::MatrixXd::Identity(10, 10); // 10 for number of digits
+	for (int i=0; i<m; ++i) {
+		one_hot_Y.row(i) = identity.row(Y(0, i));
+	}
+	one_hot_Y = one_hot_Y.transpose();
+	return one_hot_Y;
+}
+
+Eigen::MatrixXd ReLU_deriv(Eigen::MatrixXd Z) {
+	return (Z.array() > 0).cast<double>();
+}
+
+std::tuple<Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd>
+back_prop(Eigen::MatrixXd Z1, Eigen::MatrixXd A1, Eigen::MatrixXd Z2, Eigen::MatrixXd A2, Eigen::MatrixXd W1, Eigen::MatrixXd W2, Eigen::MatrixXd X, Eigen::MatrixXd Y) {
+	double m = Y.size();
+	Eigen::MatrixXd one_hot_Y = one_hot(Y);
+	Eigen::MatrixXd dZ2 = A2 - one_hot_Y;
+	Eigen::MatrixXd dW2 = 1 / m * dZ2 * A1.transpose();
+	Eigen::MatrixXd db2 = 1 / m * dZ2.rowwise().sum();
+	Eigen::MatrixXd dZ1 = W2.transpose() * dZ2 * ReLU_deriv(Z1);
+	Eigen::MatrixXd dW1 = 1 / m * dZ1 * X.transpose();
+	Eigen::MatrixXd db1 = 1 / m * dZ1.rowwise().sum();
+	return {dW1, db1, dW2, db2};
+}
+
+std::tuple<Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd>
+update_params(Eigen::MatrixXd W1, Eigen::MatrixXd b1, Eigen::MatrixXd W2, Eigen::MatrixXd b2, Eigen::MatrixXd dW1, Eigen::MatrixXd db1, Eigen::MatrixXd dW2, Eigen::MatrixXd db2, double alpha) {
+	W1 = W1 - alpha * dW1;
+	b1 = b1 - alpha * db1;
+	W2 = W2 - alpha * dW2;
+	b2 = b2 - alpha * db2;
+	return {W1, b1, W2, b2};
 }
 
 int main (int argc, char* argv[]) {
