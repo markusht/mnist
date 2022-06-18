2856a43d556dae3b890e81ba9c2772f842b4a3e6
Markus Heinrich Toribio <markusht1@gmail.com>
2022-06-18 17:49:41 +0200

    Re-implement softmax and ReLU. Issue persists.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
+#include <cmath>
 
 #include "csv_util.h"
 
-std::tuple<MatrixXdR, Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd>
+std::tuple<MatrixXdR, MatrixXdR, MatrixXdR, MatrixXdR>
 init_params() {
 	std::cerr << "Initializing parameters" << std::endl;
-	MatrixXdR W1 = Eigen::MatrixXd::Random(10,784)*0.5;
-	MatrixXdR b1 = Eigen::MatrixXd::Random(10, 1)*0.5;
-	MatrixXdR W2 = Eigen::MatrixXd::Random(10,10)*0.5;
-	MatrixXdR b2 = Eigen::MatrixXd::Random(10, 1)*0.5;
+	MatrixXdR W1 = MatrixXdR::Random(10,784)*0.5;
+	MatrixXdR b1 = MatrixXdR::Random(10, 1)*0.5;
+	MatrixXdR W2 = MatrixXdR::Random(10,10)*0.5;
+	MatrixXdR b2 = MatrixXdR::Random(10, 1)*0.5;
 	return {W1, b1, W2, b2};
 }
 
-std::tuple<MatrixXdR, Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd>
+std::tuple<MatrixXdR, MatrixXdR, MatrixXdR, MatrixXdR>
 read_data() {
 	const std::string trainPath = "../data/train.csv";
 	const std::string testPath = "../data/test.csv";
@@ -41,19 +42,20 @@ read_data() {
 	return {Y_dev, X_dev, Y_train, X_train};
 }
 
-MatrixXdR ReLU(Eigen::MatrixXd Z) {
-	return Z.cwiseMax(0);
+MatrixXdR ReLU(MatrixXdR Z) {
+	return (Z.array() > 0).cast<double>() * Z.array();
 }
 
-MatrixXdR softmax(Eigen::MatrixXd Z) {
-	MatrixXdR m = Z.array().exp();
-	double sum = m.sum();
-	MatrixXdR n = m / sum;
-	return n;
+MatrixXdR softmax(MatrixXdR Z) {
+	//oj = exp (zj - m - log{sum_i{exp(zi-m)}})
+	double m = Z.maxCoeff();
+	MatrixXdR log_oj = (Z.array() - m).array() - log((Z.array()-m).array().exp().sum());
+	MatrixXdR oj = (log_oj).array().exp();
+	return oj;
 }
 
-std::tuple<MatrixXdR, Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd>
-forward_prop(MatrixXdR W1, Eigen::MatrixXd b1, Eigen::MatrixXd W2, Eigen::MatrixXd b2, Eigen::MatrixXd X) {
+std::tuple<MatrixXdR, MatrixXdR, MatrixXdR, MatrixXdR>
+forward_prop(MatrixXdR W1, MatrixXdR b1, MatrixXdR W2, MatrixXdR b2, MatrixXdR X) {
 	MatrixXdR Z1 = W1 * X + b1.replicate(1, X.cols());
 	MatrixXdR A1 = ReLU(Z1);
 	MatrixXdR Z2 = W2 * A1 + b2.replicate(1, A1.cols());
@@ -61,10 +63,10 @@ forward_prop(MatrixXdR W1, Eigen::MatrixXd b1, Eigen::MatrixXd W2, Eigen::Matrix
 	return {Z1, A1, Z2, A2};
 }
 
-MatrixXdR one_hot(Eigen::MatrixXd Y) {
+MatrixXdR one_hot(MatrixXdR Y) {
 	int m = Y.size();
-	MatrixXdR one_hot_Y = Eigen::MatrixXd::Zero(m, 10); // because of 10 digits
-	MatrixXdR identity = Eigen::MatrixXd::Identity(10, 10); // because of 10 digits
+	MatrixXdR one_hot_Y = MatrixXdR::Zero(m, 10); // because of 10 digits
+	MatrixXdR identity = MatrixXdR::Identity(10, 10); // because of 10 digits
 	for (int i=0; i<m; ++i) {
 		one_hot_Y.row(i) = identity.row(Y(0, i));
 	}
@@ -72,12 +74,12 @@ MatrixXdR one_hot(Eigen::MatrixXd Y) {
 	return one_hot_Y;
 }
 
-MatrixXdR ReLU_deriv(Eigen::MatrixXd Z) {
+MatrixXdR ReLU_deriv(MatrixXdR Z) {
 	return (Z.array() > 0).cast<double>();
 }
 
-std::tuple<MatrixXdR, Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd>
-backward_prop(MatrixXdR Z1, Eigen::MatrixXd A1, Eigen::MatrixXd Z2, Eigen::MatrixXd A2, Eigen::MatrixXd W1, Eigen::MatrixXd W2, Eigen::MatrixXd X, Eigen::MatrixXd Y) {
+std::tuple<MatrixXdR, MatrixXdR, MatrixXdR, MatrixXdR>
+backward_prop(MatrixXdR Z1, MatrixXdR A1, MatrixXdR Z2, MatrixXdR A2, MatrixXdR W1, MatrixXdR W2, MatrixXdR X, MatrixXdR Y) {
 	double m = Y.size();
 	MatrixXdR one_hot_Y = one_hot(Y);
 	MatrixXdR dZ2 = A2 - one_hot_Y;
@@ -89,12 +91,12 @@ backward_prop(MatrixXdR Z1, Eigen::MatrixXd A1, Eigen::MatrixXd Z2, Eigen::Matri
 	return {dW1, db1, dW2, db2};
 }
 
-std::tuple<MatrixXdR, Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd>
-update_params(MatrixXdR W1, Eigen::MatrixXd b1, Eigen::MatrixXd W2, Eigen::MatrixXd b2, Eigen::MatrixXd dW1, Eigen::MatrixXd db1, Eigen::MatrixXd dW2, Eigen::MatrixXd db2, double alpha) {
-	W1 = W1 - alpha * dW1;
-	b1 = b1 - alpha * db1;
-	W2 = W2 - alpha * dW2;
-	b2 = b2 - alpha * db2;
+std::tuple<MatrixXdR, MatrixXdR, MatrixXdR, MatrixXdR>
+update_params(MatrixXdR W1, MatrixXdR b1, MatrixXdR W2, MatrixXdR b2, MatrixXdR dW1, MatrixXdR db1, MatrixXdR dW2, MatrixXdR db2, double alpha) {
+	W1 -= alpha * dW1;
+	b1 -= alpha * db1;
+	W2 -= alpha * dW2;
+	b2 -= alpha * db2;
 	return {W1, b1, W2, b2};
 }
 
@@ -112,14 +114,17 @@ double get_accuracy(Eigen::VectorXi predictions, MatrixXdR Y) {
 	return results.sum() / (double)Y.size();
 }
 
-std::tuple<MatrixXdR, Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd>
-gradient_descent(MatrixXdR X, Eigen::MatrixXd Y, double alpha, int iterations) {
+std::tuple<MatrixXdR, MatrixXdR, MatrixXdR, MatrixXdR>
+gradient_descent(MatrixXdR X, MatrixXdR Y, double alpha, int iterations) {
 	MatrixXdR W1, b1, W2, b2;
+	W1.setZero();b1.setZero();W2.setZero();b2.setZero();
 	std::tie(W1, b1, W2, b2) = init_params();
 	for (int i=0; i<iterations; ++i) {
 		MatrixXdR Z1, A1, Z2, A2;
+		Z1.setZero();A1.setZero();Z2.setZero();A2.setZero();
 		std::tie(Z1, A1, Z2, A2) = forward_prop(W1, b1, W2, b2, X);
 		MatrixXdR dW1, db1, dW2, db2;
+		dW1.setZero();db1.setZero();dW2.setZero();db2.setZero();
 		std::tie(dW1, db1, dW2, db2) = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y);
 		std::tie(W1, b1, W2, b2) = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha);
 		if (i%1==0) {
@@ -137,6 +142,22 @@ int main (int argc, char* argv[]) {
 
 	MatrixXdR W1, b1, W2, b2;
 	std::tie(W1, b1, W2, b2) = gradient_descent(X_train, Y_train, 0.10, 500);
+
+	//// For Testing
+	//MatrixXdR test = MatrixXdR::Random(10,10)*1000;
+	//int i = -50;
+	//for (int m = 0; m<10; ++m) {
+		//for (int n = 0; n<10; ++n) {
+			//test(m,n) = i;
+			//++i;
+		//}
+	//}
+	//std::cout << "Unmodified array" << std::endl;
+	//std::cout << test << std::endl;
+	//std::cout << "Softmax" << std::endl;
+	//std::cout << softmax(test) << std::endl;
+	//std::cout << "ReLU" << std::endl;
+	//std::cout << ReLU(test) << std::endl;
 
 	return 0;
 }
